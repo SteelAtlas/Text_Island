c41643fe3bbf9342a7959f0530114e6801c181e0
ravikul505 <amanogawaeiko000@gmail.com>
2023-07-11 19:57:58 +0530

    sex

---
+++ Kilo.c
@@ -0,0 +1,131 @@
+/***includes***/
+#include <ctype.h>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include<sys/ioctl.h>
+
+/***Defines***/
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/***Data***/
+struct editorConfig{
+
+	int screenrows;
+	int screencols;
+	struct termios orig_termios;
+};
+struct editorConfig E;
+
+/***terminal***/
+void enableRawMode() ;
+void disableRawMode();
+void die(const char *s);
+void editorProcessKeypress();
+int getWindowSize(int* rows, int* cols);
+
+/***input***/
+char editorReadKey();
+
+/***output***/
+void editorRefreshScreen();
+void editorDrawRows();
+
+/***Init***/
+void initditor(){
+	if(getWindowSize(&E.screenrows,&E.screencols) == -1){die("gwtWindowSize");}
+}
+int main() {
+
+	initditor();
+	enableRawMode();
+
+	char c;
+	while (1)
+	{
+		editorRefreshScreen();
+		editorProcessKeypress();
+	}
+	return 0;
+}
+/*---------------------------------------------------*/
+
+int getWindowSize(int* rows , int* cols){
+	struct winsize ws;
+	if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){return -1;}
+	else 
+	{
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+}
+
+void editorDrawRows(){
+	int y;
+	for(y = 0 ; y < E.screenrows ; y++)
+	{
+		write(STDOUT_FILENO ,"~\r\n" , 3);
+	}
+}
+
+void editorRefreshScreen(){
+	write(STDOUT_FILENO, "\x1b[2J",4);
+	write(STDOUT_FILENO,"\x1b[H",3);
+
+	editorDrawRows();
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+char editorReadKey(){
+	int nread;
+	char c ;
+	while((nread = read(STDIN_FILENO, &c,1)) != 1){
+		if(nread == -1 && errno != EAGAIN) {die("read");}
+	}
+
+	return c;
+}
+
+void editorProcessKeypress(){
+	char c = editorReadKey();
+	switch (c) {
+		case CTRL_KEY('q'):
+			write(STDOUT_FILENO, "\x1b[2J",4 );
+			write(STDOUT_FILENO, "\x1b[H", 3);
+			exit(0);
+		break;
+	}
+}
+
+void enableRawMode(){
+	
+	if(tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) {die("tcsgetattr");}	
+	atexit(disableRawMode);
+	
+  	struct termios raw = E.orig_termios;
+	raw.c_iflag &= ~(BRKINT | ICRNL| INPCK| ISTRIP | IXON);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+  	raw.c_lflag &= ~(ECHO | ICANON |IEXTEN | ISIG);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 5;
+
+  	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {die("tcsetattr");}
+};
+
+
+void disableRawMode(){
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {die("tcssetattr");}	
+};
+
+void die(const char *s){
+	write(STDOUT_FILENO,"\x1b[2j",4);
+	write(STDOUT_FILENO,"\x1b[H",3);
+	perror(s);
+	exit(1);
+};
+
