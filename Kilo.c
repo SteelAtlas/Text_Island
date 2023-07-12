2a30fee8a0c44660ba6f6da5efbc09129bf91d39
SteelAtlas <sawzeno57@outlook.com>
2023-07-12 12:04:57 +0530

    Boobs

--- Kilo.c
+++ Kilo.c
@@ -25,6 +25,7 @@ void disableRawMode();
 void die(const char *s);
 void editorProcessKeypress();
 int getWindowSize(int* rows, int* cols);
+int getCursorPosition(int* rows , int* cols);
 
 /***input***/
 char editorReadKey();
@@ -34,14 +35,14 @@ void editorRefreshScreen();
 void editorDrawRows();
 
 /***Init***/
-void initditor(){
-	if(getWindowSize(&E.screenrows,&E.screencols) == -1){die("gwtWindowSize");}
+void initeditor(){
+	if(getWindowSize(&E.screenrows,&E.screencols) == -1){die("getWindowSize");}
 }
+
 int main() {
 
-	initditor();
+	initeditor();
 	enableRawMode();
-
 	char c;
 	while (1)
 	{
@@ -52,9 +53,32 @@ int main() {
 }
 /*---------------------------------------------------*/
 
+int getCursorPosition(int* rows , int* cols){
+	char buf[32];
+	unsigned int i = 0;
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+	while (i < sizeof(buf) - 1)
+	{
+		if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+		if (buf[i] == 'R') break;
+		i++;
+	}
+
+	buf[i] = '\0';
+	if(buf[0] != '\x1b' || buf[1] != '[') return -1;
+	if(sscanf(&buf[2],"%d;%d", rows, cols) != 2) return -1;
+
+	return 0;
+}
+
 int getWindowSize(int* rows , int* cols){
 	struct winsize ws;
-	if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){return -1;}
+	if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+	{
+		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) {return -1;}
+		return getCursorPosition(rows,cols);
+	}
 	else 
 	{
 		*cols = ws.ws_col;
@@ -67,7 +91,9 @@ void editorDrawRows(){
 	int y;
 	for(y = 0 ; y < E.screenrows ; y++)
 	{
-		write(STDOUT_FILENO ,"~\r\n" , 3);
+		write(STDIN_FILENO , "~" , 1);
+
+		if( y < E.screenrows -1){write(STDIN_FILENO , "\r\n" , 2);}
 	}
 }
 
